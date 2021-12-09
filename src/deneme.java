d63500ab3c552793d0520e719cb6e3ea7ad08232
kemalyayil <82785245+kemalyayil@users.noreply.github.com>
2021-12-08 22:40:07 -0300

    ooahah

---
+++ src/deneme.java
@@ -0,0 +1,11 @@
+public class deneme {
+    public static void main(String[] args) {
+        //1.satir
+        //2.satir
+        //3.satir
+        //4.satir
+        //5.satir
+        //6.satir
+
+    }
+}
